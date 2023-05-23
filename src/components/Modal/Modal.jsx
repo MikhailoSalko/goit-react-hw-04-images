6c353321b2480ceeacb074a2bfe1321458461dcb
Mikhailo Salko <slipknotmisha@gmail.com>
2023-05-23 13:42:50 +0300

    refactoring modal using react hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,35 @@
+import { useCallback, useEffect } from 'react';
 import { createPortal } from 'react-dom';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.closeModal);
-  }
+function Modal({ children, onClose }) {
+  const closeModal = useCallback(
+    ({ target, currentTarget, code }) => {
+      // console.log(code);
+      if (target === currentTarget || code === 'Escape') {
+        onClose();
+      }
+    },
+    [onClose]
+  );
+  useEffect(() => {
+    document.addEventListener('keydown', closeModal);
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.closeModal);
-  }
+    return () => {
+      document.removeEventListener('keydown', closeModal);
+    };
+  }, [closeModal]);
 
-  closeModal = ({ target, currentTarget, code }) => {
-    // console.log(code);
-    if (target === currentTarget || code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  render() {
-    const { children } = this.props;
-    return createPortal(
-      <div className={styles.overlay} onClick={this.closeModal}>
-        <div className={styles.modal}>{children}</div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className={styles.overlay} onClick={closeModal}>
+      <div className={styles.modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
 }
 
 export default Modal;
